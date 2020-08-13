b39c956b2a6d4020b5954776cea62b02580c20fa
Mike Koshakow <mzkosh@gmail.com>
2020-08-13 16:45:34 -0400

    serving home page

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ module.exports = app;
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname), '../public'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
