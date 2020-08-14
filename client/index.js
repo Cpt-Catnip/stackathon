6db3f126596765b175f172b7145f96ac0dc340ce
Mike Koshakow <mzkosh@gmail.com>
2020-08-14 17:01:40 -0400

    dropping ball

--- client/index.js
+++ client/index.js
@@ -1,16 +1,20 @@
 import { Engine } from 'matter-js';
-import { app, engine, particle } from './game';
+import { app, engine, Particle } from './game';
 
 let particles = [];
 
 const setup = () => {
-  particles.push(new particle(10, 10, 20));
-  Engine.run(engine)
+  // add assets to game
+  document.getElementById('app').appendChild(app.view);
+  particles.push(new Particle(10, 10, 20));
+
+  // start game loop
+  Engine.run(engine);
   app.ticker.add((delta) => gameLoop(delta));
 };
 
 const gameLoop = (delta) => {
-  particle.forEach((particle) => particle.show());
+  particles.forEach((particle) => particle.show());
 };
 
 setup();
